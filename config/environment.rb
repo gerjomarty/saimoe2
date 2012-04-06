54d9316034b0c60bea1360de14d1dab8d30afcff
Gerard Martin <gerjomarty@anientity.co.uk>
2012-04-06 15:39:42 +0100

    Generate a secret token for test env, and only rely on development one in development.

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,5 @@ require File.expand_path('../../lib/foreign_key_migration', __FILE__)
 require File.expand_path('../../lib/nullify_blank_attributes', __FILE__)
 require File.expand_path('../../lib/column_methods', __FILE__)
 
-ENV['SECRET_TOKEN'] = File.read File.expand_path('../../lib/data/secret_token', __FILE__) unless Rails.env.production?
-
 # Initialize the rails application
 Saimoe2::Application.initialize!
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,6 +1,8 @@
 Saimoe2::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  ENV['SECRET_TOKEN'] = '869311e2fd552bf0f0d04883566042f545e96075a122b8659b9295956d25b20a48eb7f6300031afc2c634297fc12468687daf6afd1b94b5738b58b2dddf59ea4'
+
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
