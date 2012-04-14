d11bd52ec8224df4d20dec0f9a027c9c218db95f
Gerard Martin <gerjomarty@anientity.co.uk>
2012-04-14 23:05:21 +0100

    Added validation for character once per tournament.

--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -7,4 +7,15 @@ class Appearance < ActiveRecord::Base
   has_many :voice_actor_roles, inverse_of: :appearance
 
   validates :character_role_id, :tournament_id, presence: true, numericality: {only_integer: true}
+  validate :one_appearance_per_tournament
+
+  private
+
+  def one_appearance_per_tournament
+    apps = Appearance.joins(:character_role)
+                     .where(character_roles: {character_id: character_role && character_role.character_id})
+    if apps.any? {|app| app.id != self.id && app.tournament_id == self.tournament_id}
+      errors.add :base, "Character can only appear once per tournament"
+    end
+  end
 end
--- spec/models/appearance_spec.rb
+++ spec/models/appearance_spec.rb
@@ -10,4 +10,29 @@ describe Appearance do
   it { should validate_numericality_of(:character_role_id).only_integer }
   it { should validate_presence_of(:tournament_id) }
   it { should validate_numericality_of(:tournament_id).only_integer }
+
+  describe "should only allow one character appearance per tournament" do
+    before :each do
+      @tournament = create :tournament
+      @char_role = create :character_role
+      create :appearance, tournament: @tournament, character_role: @char_role
+    end
+
+    it "with same character role" do
+      app = build :appearance, tournament: @tournament, character_role: @char_role
+
+      app.valid?.should be_false
+      app.errors.size.should == 1
+      app.errors[:base][0].should == "Character can only appear once per tournament"
+    end
+
+    it "with differing character roles" do
+      char_role = create :character_role, character: @char_role.character
+      app = build :appearance, tournament: @tournament, character_role: char_role
+
+      app.valid?.should be_false
+      app.errors.size.should == 1
+      app.errors[:base][0].should == "Character can only appear once per tournament"
+    end
+  end
 end
