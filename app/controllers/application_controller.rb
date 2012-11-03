2395cccd5851417ffcad989ae291b6f2d99988f3
Gerard Martin <gerjomarty@anientity.co.uk>
2012-11-03 15:48:46 +0000

    Revert change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :force_http
-
-  def force_http
-    redirect_to protocol: 'http://'
-    true
-  end
 end
