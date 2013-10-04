caac471ade7821a0064b641162ec9ab9fd4621bf
Gerard Martin <gerard.martin@118118.com>
2013-10-04 15:41:41 +0100

    Added late message, removed old update.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,8 +8,16 @@ class HomeController < ApplicationController
                                              MatchViewModel.new(m, cache: :home_most_recent, match_name: :short, show_percentages: true, match_entry_ordering: :ordered_by_votes)
                                            end
 
+    today = Time.zone.now.advance(hours: 1).to_date
+
+    if Match.where(is_finished: false, date: Match.date_before(today)).any?
+      @late_message = "Latest results will be updated here as soon as possible."
+    else
+      @late_message = nil
+    end
+
     @todays_match_view_models =
-      if (scope = Match.ordered_by_date.where(is_finished: false, date: Time.zone.now.advance(hours: 1).to_date).scoped).any?
+      if (scope = Match.ordered_by_date.where(is_finished: false, date: today).scoped).any?
         scope.collect {|m| MatchViewModel.new(m, cache: :home_todays_matches, match_name: :short, schema_markup: true) }
       else
         nil
