b0c93c6bc126f13adf86b596eca512d6acaf3aec
Gerard Martin <gerjomarty@anientity.co.uk>
2013-11-19 01:19:46 +0000

    Semi-finals template

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -171,10 +171,10 @@ class Admin::AdminController < ApplicationController
         }.collect {|id_string, id_name, id_series|
           char_index = character_arr.index {|j_name, _| j_name == id_name} if id_name
           series_index = series_arr.index {|j_series, _| j_series == id_series} if id_series
-          j_name, e_name, char_code, va, va_code, prelim_rank, prev_best, r1_vote_rank, r1_votes, r1_percent_rank, r1_percent, r2_vote_rank, r2_votes, r2_percent_rank, r2_percent, r3_vote_rank, r3_votes, r3_percent_rank, r3_percent, r4_vote_rank, r4_votes, r4_percent_rank, r4_percent, playoff_votes, r5_vote_rank, r5_votes, r5_percent_rank, r5_percent, total_votes_rank, total_votes_number, nickname, defeated = nil
+          j_name, e_name, char_code, va, va_code, prelim_rank, prev_best, r1_vote_rank, r1_votes, r1_percent_rank, r1_percent, r2_vote_rank, r2_votes, r2_percent_rank, r2_percent, r3_vote_rank, r3_votes, r3_percent_rank, r3_percent, r4_vote_rank, r4_votes, r4_percent_rank, r4_percent, playoff_votes, r5_vote_rank, r5_votes, r5_percent_rank, r5_percent, r6_vote_rank, r6_votes, r6_percent_rank, r6_percent, total_votes_rank, total_votes_number, nickname, defeated = nil
           if char_index
-            j_name, e_name, char_code, va, va_code, prelim_rank, prev_best, r1_vote_rank, r1_votes, r1_percent_rank, r1_percent, r2_vote_rank, r2_votes, r2_percent_rank, r2_percent, r3_vote_rank, r3_votes, r3_percent_rank, r3_percent, r4_vote_rank, r4_votes, r4_percent_rank, r4_percent, playoff_votes, r5_vote_rank, r5_votes, r5_percent_rank, r5_percent, total_votes_rank, total_votes_number, nickname = character_arr[char_index]
-            defeated = character_arr[char_index][31..-2]
+            j_name, e_name, char_code, va, va_code, prelim_rank, prev_best, r1_vote_rank, r1_votes, r1_percent_rank, r1_percent, r2_vote_rank, r2_votes, r2_percent_rank, r2_percent, r3_vote_rank, r3_votes, r3_percent_rank, r3_percent, r4_vote_rank, r4_votes, r4_percent_rank, r4_percent, playoff_votes, r5_vote_rank, r5_votes, r5_percent_rank, r5_percent, r6_vote_rank, r6_votes, r6_percent_rank, r6_percent, total_votes_rank, total_votes_number, nickname = character_arr[char_index]
+            defeated = character_arr[char_index][35..-2]
             defeated = nil if defeated && defeated.empty?
           end
           j_series, e_series, series_color, series_code = nil
@@ -188,7 +188,7 @@ class Admin::AdminController < ApplicationController
               _, _, series_color, series_code = series_arr[series_index]
             end
           end
-          [id_string, j_name, e_name, j_series, e_series, char_code, va, va_code, prelim_rank, prev_best, series_color, series_code, r1_vote_rank, r1_votes, r1_percent_rank, r1_percent, r2_vote_rank, r2_votes, r2_percent_rank, r2_percent, r3_vote_rank, r3_votes, r3_percent_rank, r3_percent, r4_vote_rank, r4_votes, r4_percent_rank, r4_percent, playoff_votes, r5_vote_rank, r5_votes, r5_percent_rank, r5_percent, total_votes_rank, total_votes_number, nickname, defeated]
+          [id_string, j_name, e_name, j_series, e_series, char_code, va, va_code, prelim_rank, prev_best, series_color, series_code, r1_vote_rank, r1_votes, r1_percent_rank, r1_percent, r2_vote_rank, r2_votes, r2_percent_rank, r2_percent, r3_vote_rank, r3_votes, r3_percent_rank, r3_percent, r4_vote_rank, r4_votes, r4_percent_rank, r4_percent, playoff_votes, r5_vote_rank, r5_votes, r5_percent_rank, r5_percent, r6_vote_rank, r6_votes, r6_percent_rank, r6_percent, total_votes_rank, total_votes_number, nickname, defeated]
         }.each {|result|
           if result[0].present?
             sr[-1] << result
@@ -218,7 +218,7 @@ class Admin::AdminController < ApplicationController
       }.join("<br /><br />\r\n\r\n")
 
       template_names = @split_names.collect do |s_result|
-        s_result.collect do |_, j_name, e_name, j_series, e_series, char_code, va, va_code, prelim_rank, prev_best, series_color, series_code, r1_vote_rank, r1_votes, r1_percent_rank, r1_percent, r2_vote_rank, r2_votes, r2_percent_rank, r2_percent, r3_vote_rank, r3_votes, r3_percent_rank, r3_percent, r4_vote_rank, r4_votes, r4_percent_rank, r4_percent, playoff_votes, r5_vote_rank, r5_votes, r5_percent_rank, r5_percent, total_votes_rank, total_votes_number, nickname, defeated|
+        s_result.collect do |_, j_name, e_name, j_series, e_series, char_code, va, va_code, prelim_rank, prev_best, series_color, series_code, r1_vote_rank, r1_votes, r1_percent_rank, r1_percent, r2_vote_rank, r2_votes, r2_percent_rank, r2_percent, r3_vote_rank, r3_votes, r3_percent_rank, r3_percent, r4_vote_rank, r4_votes, r4_percent_rank, r4_percent, playoff_votes, r5_vote_rank, r5_votes, r5_percent_rank, r5_percent, r6_vote_rank, r6_votes, r6_percent_rank, r6_percent, total_votes_rank, total_votes_number, nickname, defeated|
           {name: e_name || j_name,
            series: e_series || j_series,
            character_code: char_code,
@@ -249,6 +249,10 @@ class Admin::AdminController < ApplicationController
            round_5_votes: r5_votes,
            round_5_percentage_rank: r5_percent_rank,
            round_5_percentage: r5_percent,
+           round_6_vote_rank: r6_vote_rank,
+           round_6_votes: r6_votes,
+           round_6_percentage_rank: r6_percent_rank,
+           round_6_percentage: r6_percent,
            total_votes_rank: total_votes_rank,
            total_votes: total_votes_number,
            nickname: nickname,
@@ -286,6 +290,9 @@ class Admin::AdminController < ApplicationController
       elsif info[:name_round_6_matches] == '1'
         template = Template.new(names: template_names, result_list: name_list)
                            .render(File.read(Rails.root.join('lib', 'templates', 'round_6_matches.html.erb')))
+      elsif info[:name_round_7_matches] == '1'
+        template = Template.new(names: template_names, result_list: name_list)
+                           .render(File.read(Rails.root.join('lib', 'templates', 'round_7_matches.html.erb')))
       else
         template = Template.new(result_list: name_list)
                            .render(File.read(Rails.root.join('lib', 'templates', 'result_list.html.erb')))
